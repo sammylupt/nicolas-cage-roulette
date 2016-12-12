4433751fa108fdd9a35e5d2e37e075f3d7e2c300
Scott Luptowski <scottluptowski@gmail.com>
2016-12-12 09:44:36 -0500

    update list

--- js/main.js
+++ js/main.js
@@ -21,28 +21,24 @@ policies for that. Any broken links, let me know */
       70206627, // trespass
       80005641, // rage
       16915411, // snake eyes
-      60000959, // gone in 60 seconds
-      70000207, // kiss of death
-      70011206, // national treasure
-      70038796, // lord of war
-      70135880, // seeking justice
       70257818, // stolen
-      80013773, // left behind
       80015345, // outcast
-      80074515  // pay the ghost
+      80063773, // the trust
+      70135880, // seeking justice
+      914950 // the rock
     ];
 
     /*the "All Movies" link has a data-cage attr. If this link is clicked,
     add those movies */
-    
     if (this.getAttribute('data-cage')) {
       movies.concat([
-        70023955, // the weatherman
+        70048301, // the wicker man
+        80074515, // pay the ghost
         80057700  // the runner
       ]);
     } 
 
     var random = Math.floor(Math.random() * movies.length);
-    document.location = "https://movies.netflix.com/WiPlayer?movieid=" + movies[random];
+    document.location = "http://www.netflix.com/watch/" + movies[random];
   }
 })()
